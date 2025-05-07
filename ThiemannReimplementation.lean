7636e16783d97e6d1e41cdd736474ff8a46a638a
AraneusRota <44680457+AraneusRota@users.noreply.github.com>
2025-05-07 13:28:50 +0200

    thiemann 2.1: commands with callbacks

--- ThiemannReimplementation.lean
+++ ThiemannReimplementation.lean
@@ -9,23 +9,52 @@ deriving DecidableEq, BEq, Repr
 open Type'
 
 inductive Session
-  | send : Type' -> Session -> Session
-  | recv : Type' -> Session -> Session
-  | ends : Session
+  | _! : Type' -> Session -> Session
+  | _? : Type' -> Session -> Session
+  | _end : Session
 open Session
 
 #check (·  + 1)
 
-def binaryp := recv int $ recv int $ recv int ends 
-def unaryp := recv int $ send int ends
+def binaryp := _? int $ _? int $ _? int _end 
+def unaryp := _? int $ _! int _end
 
 
 def dual
-    | send t s => recv t $ dual s
-    | recv t s => send t $ dual s
-    | ends => ends
+    | _! t s => _? t $ dual s
+    | _? t s => _! t $ dual s
+    | _end => _end
 
 #eval dual unaryp
 
 
 -- 2.1 Commands with Callbacks
+
+abbrev Type'.de
+    | int => Int
+    | bool => Bool
+#check bool.de
+ 
+inductive Cmd (State : Type) : Session -> Type
+    | close : Cmd State _end
+    | send : 
+        (State -> State × de Payload) -> 
+            Cmd State ContinuationSession -> 
+                Cmd State (_! Payload ContinuationSession)
+    | recv : 
+        (de Payload -> State -> State) -> 
+            Cmd State ContinuationSession -> 
+                Cmd State (_? Payload ContinuationSession)
+open Cmd
+    
+-- def negateServer : Cmd Int (_? int _end) := 
+    -- recv (fun payload state => payload) close
+    
+def negateServer := 
+    recv (fun (operand : int.de) _ => operand) <| 
+        send (fun state => (state, -state)) close
+
+def additionServer := 
+    recv (fun (l : int.de) _ => l) <|
+        recv (fun r l => l + r) <|
+            send (fun sum => (sum, sum)) close
